00ed23d6aaff7bcaaa87fd71db51b9e2536561db
Codetancy <codetancy@gmail.com>
2022-09-20 01:44:15 -0400

    Set default dir to current dir

--- Dotree/Program.cs
+++ Dotree/Program.cs
@@ -1,10 +1,6 @@
-﻿using Dotree;
+﻿using System.IO;
+using Dotree;
 
-var currentDirectory = "C:\\Users\\frank\\dev\\Dotree\\Dotree";// Directory.GetCurrentDirectory();
+var currentDirectory = Directory.GetCurrentDirectory();
 var tree = FileTree.Create(currentDirectory);
 tree.Display();
-// // string lightRed = "\x1b[1;31m";
-// // Console.WriteLine($"{lightRed}Hello");
-// Console.WriteLine("\u251C");
-// Console.WriteLine("\u2500");
-// Console.WriteLine("\u2502");
